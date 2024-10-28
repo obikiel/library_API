1f4ab02ef923905fbc5fceb299dc0f5c15753773
obioma chijindu ezekiel <ezekielobiomachi1@gmail.com>
2024-10-28 18:22:34 +0100

    Update settings.py

--- library/settings.py
+++ library/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^lj@9i6kgdc%%#xwk&5p4$#1!ubhfz)w8gtamn(t_dne9@k@t=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
